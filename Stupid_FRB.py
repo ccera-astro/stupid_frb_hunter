4e9c829bf069be6b2a62f4d55130b098d6f11d1d
Marcus Leech <mleech@ripnet.com>
2021-02-06 23:22:36 -0500

    Changed detection strategy to be *much* cheaper, given super-bright   FRBs.

--- Stupid_FRB.py
+++ Stupid_FRB.py
@@ -13,9 +13,10 @@ import json
 import math
 
 class blk(gr.sync_block):  # other base classes are basic_block, decim_block, interp_block
-    """A pulsar folder/de-dispersion block"""
+    """A FRB first-order detector block"""
 
-    def __init__(self, fbsize=16,filename='/dev/null',fbrate=2500,dms=[10.0,20.0,30.0,40.0,50.0],freq=408.0e6,bw=2.56e6,thresh=5.0):  # only default arguments here
+    def __init__(self, fbsize=16,filename='/dev/null',fbrate=2500,chans=[1,7,14],
+        thresh=5.0,minsmear=2.5e-3):  # only default arguments here
         """arguments to this function show up as parameters in GRC"""
         gr.sync_block.__init__(
             self,
@@ -26,24 +27,30 @@ class blk(gr.sync_block):  # other base classes are basic_block, decim_block, in
         # if an attribute with the same name as a parameter is found,
         # a callback is registered (properties work, too).
         
-        self.dmtable=dms
-        self.filespec=filename
-        self.flen = fbsize
-        self.maxdelays = []
-        self.delayincrs = []
-        flower = (freq-(bw/2.0))/1.0e6
-        fupper = (freq+(bw/2.0))/1.0e6
-        self.ndms = len(self.dmtable)
-        for dm in self.dmtable:
-            smear = dm/2.41e-4 * (1.0/(flower*flower)-1.0/(fupper*fupper))
-            smear = int(round(smear * fbrate))
-            incr = int(round(float(smear)/float(fbsize)))
-            self.maxdelays.append(smear)
-            self.delayincrs.append(incr)
-        self.obufs = np.zeros((self.ndms+1,fbrate))
-        self.avgs = [0.0]*(self.ndms+1)
-        self.ocount = 0
+        self.filename = filename
+        self.two_seconds = int(fbrate*2)
+        self.channels=[]
+        self.chans = chans
+        self.lchans = len(chans)
+        self.scnt = 0
+        self.mindistance = minsmear * float(fbrate)
+        self.mindistance = int(self.mindistance)
+        for x in range(fbsize):
+            self.channels.append([0.0]*self.two_seconds)
+            
         self.thresh = thresh
+        self.flen = fbsize
+
+    def logthispuppy(self):
+        q = self.thresh
+        ltp = time.gmtime()
+        fn = self.filename+"-"
+        fn = fn + "%04d%02d%02d.%02d%02d" % (ltp.tm_year, ltp.tm_mon,
+            ltp.tm_mday, ltp.tm_hour, ltp.tm_min)
+        fp = open (fn+".dat", "w")
+        for c in range(lchans):
+            fp.write (str(self.channels[c])+"\n")
+        fp.close()
 
     def work(self, input_items, output_items):
         """Do dedispersion/folding"""
@@ -51,56 +58,43 @@ class blk(gr.sync_block):  # other base classes are basic_block, decim_block, in
         l = len(q)
         for i in range(int(l/self.flen)):
             bndx = i*self.flen
-            
-            for x in range(self.ndms):
-            #
-            # Do delay/dedispersion logic
-            #
-                if (self.maxdelays[x] > 0):
-                    outval = 0.0
+            for c in range(self.flen):
+                self.channels[c][self.scnt] = q[bndx]
+                bndx += 1
+            self.scnt += 1
+            if (self.scnt >= self.two_seconds):
+                # Analysis
+                amused = 0
+                amusingplaces = []
+                #
+                # For each of the "test" (amusing) channels
+                #
+                # Test for a spike that is "big" compared to mean
+                #  in each of the "test" channels
+                #
+                for amusing in self.chans:
                     #
-                    # start at 1 because
-                    #  we already know maxdelay > 1
+                    # Channel average
                     #
-                    for j in range(1,self.flen):
-                        if ((self.maxdelays[x] - (self.delayincrs[x]*j)) <= 0):
-                            outval += q[bndx+j]
-                    self.maxdelays[x] -= 1
-                else:
-                    outval = sum(q[bndx:bndx+self.flen])
+                    cavg = np.mean(self.channels[amusing])
+                    if ((np.max(self.channels[amusing])) / cavg > self.thresh):
+                        amused += 1
+                        amusingplaces.append(np.argmax(self.channels[amusing]))
+                    else:
+                        break
                 
-                
-                self.obufs[x+1][self.ocount] = outval
-            
-            self.obufs[0][self.ocount] = sum(q[bndx:bndx+self.flen])
-            self.ocount += 1
-            if (self.ocount >= len(self.obufs[0])):
-                self.ocount = 0
-                
-                for x in range(self.ndms+1):
-                    avg = math.fsum(self.obufs[x])
-                    avg /= len(self.obufs[x])
-                    if (self.avgs[x] == 0):
-                        self.avgs[x] = avg
-                    avg += self.avgs[x]
-                    avg /= 2.0
-                    self.avgs[x] = avg
-                    if ((max(self.obufs[x]) > avg*self.thresh) and x != 0):
-                        t = time.gmtime()
-                        d = {}
-                        d["time"] = "%04d%02d%02d-%02d:%02d:%02d" % (t.tm_year,
-                            t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec)
-                        tseries = []
-                        for obuf in self.obufs:
-                            tseries.append(list(obuf))
-                        d["tseries"] = tseries
-                        d["dms"] = [0.0]+self.dmtable
-                        d["trigger"] = (x,d["dms"][x])
-                        fn = self.filespec+"-"+d["time"]+".json"
-                        fp = open(fn, "w")
-                        fp.write(json.dumps(d, indent=4)+"\n")
-                        fp.close()  
-                        break                  
+                #
+                # Only if we were amused in all amusing places
+                #
+                if (amused >= self.lchans):
+                    #
+                    # Spike on lower-frequency channel must occur *after*
+                    #  higher-frequency channel. Difference in positions
+                    #  must be "significant"
+                    #
+                    if (amusingplaces[0] > amusingplaces[len(amusingplaces)-1] and
+                        (amusingplaces[0] - amusingplaces[len(amusingplaces)-1]) > self.mindistance):
+                        self.logthispuppy()
+                self.scnt = 0
 
-            
         return len(q)
